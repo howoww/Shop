9b0575e39e70fb73f9983fd66545d2888d95e1e3
HowWow <yarohsyk10@gmail.com>
2024-04-22 00:20:08 +0400

    FixControllers

--- AdminController.h
+++ AdminController.h
@@ -2,47 +2,49 @@
 #include "ProductController.h"
 #include "UserController.h"
 #include "MenuView.h"
-class AdminController : public ProductController, public UserController
+class AdminController
 {
 private:
 	MenuView _menuView;
+	ProductController _productController;
+	UserController _userController;
 public:
 	AdminController() : _menuView("Главное меню", std::vector<MenuView>{
 		MenuView("Пользователи", std::vector<MenuView>{
-			MenuView("Список пользователей", [&]() {UserController::displayUsers(); system("pause"); }),
+			MenuView("Список пользователей", [&]() {_userController.displayUsers(); system("pause"); }),
 				MenuView("Поиск", std::vector<MenuView>{
-				MenuView("Поиск по ID", [&]() { UserController::searchUsers<int>(&User::getId); }),
-					MenuView("Поиск по имени", [&]() {UserController::searchUsers<std::string>(&User::getName); }),
-					MenuView("Поиск по логину", [&]() {UserController::searchUsers<std::string>(&User::getLogin); })}),
-				MenuView("Добавить пользователя", [&]() { UserController::addUser(); }),
-				MenuView("Изменть пользователя", [&]() {UserController::editUser(); }),
-				MenuView("Удалить пользователя", [&]() {UserController::deleteUser(); }),
+				MenuView("Поиск по ID", [&]() { _userController.searchUsers<int>(&User::getId); }),
+					MenuView("Поиск по имени", [&]() {_userController.searchUsers<std::string>(&User::getName); }),
+					MenuView("Поиск по логину", [&]() {_userController.searchUsers<std::string>(&User::getLogin); })}),
+				MenuView("Добавить пользователя", [&]() { _userController.addUser(); }),
+				MenuView("Изменть пользователя", [&]() {_userController.editUser(); }),
+				MenuView("Удалить пользователя", [&]() {_userController.deleteUser(); }),
 				MenuView("Сортировка", std::vector<MenuView>{
-				MenuView("Сортировать по ID", [&]() {UserController::sort<int>(&User::getId); }),
-					MenuView("Сортировать по имени", [&]() {UserController::sort<std::string>(&User::getName); }),
-					MenuView("Сортировать по логину", [&]() {UserController::sort<std::string>(&User::getLogin); })
+				MenuView("Сортировать по ID", [&]() {_userController.sort<int>(&User::getId); }),
+					MenuView("Сортировать по имени", [&]() {_userController.sort<std::string>(&User::getName); }),
+					MenuView("Сортировать по логину", [&]() {_userController.sort<std::string>(&User::getLogin); })
 			})
 		}),
 			MenuView("Товары", std::vector<MenuView>{
-			MenuView("Список товаров", [&]() {ProductController::displayProducts(); system("pause"); }),
+			MenuView("Список товаров", [&]() {_productController.displayProducts(); system("pause"); }),
 				MenuView("Поиск", std::vector<MenuView>{
-				MenuView("Поиск по ID", [&]() { ProductController::searchProducts<int>(&Product::getId); }),
-					MenuView("Поиск по наименованию", [&]() {ProductController::searchProducts<std::string>(&Product::getName); }),
-					MenuView("Поиск по категории", [&]() {ProductController::searchProducts<std::string>(&Product::getCategory); }),
-					MenuView("Поиск по марке", [&]() {ProductController::searchProducts<std::string>(&Product::getMark); }),
-					MenuView("Поиск по цене", [&]() {ProductController::searchProducts<double>(&Product::getCost); })
+				MenuView("Поиск по ID", [&]() { _productController.searchProducts<int>(&Product::getId); }),
+					MenuView("Поиск по наименованию", [&]() {_productController.searchProducts<std::string>(&Product::getName); }),
+					MenuView("Поиск по категории", [&]() {_productController.searchProducts<std::string>(&Product::getCategory); }),
+					MenuView("Поиск по марке", [&]() {_productController.searchProducts<std::string>(&Product::getMark); }),
+					MenuView("Поиск по цене", [&]() {_productController.searchProducts<double>(&Product::getCost); })
 			}),
-				MenuView("Добавить товар", [&]() {ProductController::addProduct(); }),
-				MenuView("Изменть товар", [&]() {ProductController::editProduct(); }),
-				MenuView("Удалить товар", [&]() {ProductController::deleteProduct(); }),
-				MenuView("Импорт из Csv", [&]() {ProductController::importFromCSV(); }),
-				MenuView("Экспорт в Csv", [&]() {ProductController::exportToCSV(); }),
+				MenuView("Добавить товар", [&]() {_productController.addProduct(); }),
+				MenuView("Изменть товар", [&]() {_productController.editProduct(); }),
+				MenuView("Удалить товар", [&]() {_productController.deleteProduct(); }),
+				MenuView("Импорт из Csv", [&]() {_productController.importFromCSV(); }),
+				MenuView("Экспорт в Csv", [&]() {_productController.exportToCSV(); }),
 				MenuView("Сортировка", std::vector<MenuView>{
-				MenuView("Сортировать по ID", [&]() { ProductController::sort<int>(&Product::getId); }),
-					MenuView("Сортировать по наименованию", [&]() {ProductController::sort<std::string>(&Product::getName); }),
-					MenuView("Сортировать по категории", [&]() {ProductController::sort<std::string>(&Product::getCategory); }),
-					MenuView("Сортировать по марке", [&]() {ProductController::sort<std::string>(&Product::getMark); }),
-					MenuView("Сортировать по цене", [&]() {ProductController::sort<double>(&Product::getCost); })
+				MenuView("Сортировать по ID", [&]() { _productController.sort<int>(&Product::getId); }),
+					MenuView("Сортировать по наименованию", [&]() {_productController.sort<std::string>(&Product::getName); }),
+					MenuView("Сортировать по категории", [&]() {_productController.sort<std::string>(&Product::getCategory); }),
+					MenuView("Сортировать по марке", [&]() {_productController.sort<std::string>(&Product::getMark); }),
+					MenuView("Сортировать по цене", [&]() {_productController.sort<double>(&Product::getCost); })
 			})
 		})}) {
 		system("cls");
--- DefaultUserController.h
+++ DefaultUserController.h
@@ -1,27 +1,28 @@
 #pragma once
 #include "ProductController.h"
 #include "MenuView.h"
-class DefaultUserController : public ProductController
+class DefaultUserController
 {
 private:
 	MenuView _menuView;
+	ProductController _productController;
 public:
 	DefaultUserController() : _menuView("Главное меню", std::vector<MenuView>{
 		MenuView("Товары", std::vector<MenuView>{
-			MenuView("Список товаров", [&]() {ProductController::displayProducts(); system("pause"); }),
+			MenuView("Список товаров", [&]() {_productController.displayProducts(); system("pause"); }),
 				MenuView("Поиск", std::vector<MenuView>{
-				MenuView("Поиск по ID", [&]() { ProductController::searchProducts<int>(&Product::getId); }),
-					MenuView("Поиск по наименованию", [&]() {ProductController::searchProducts<std::string>(&Product::getName); }),
-					MenuView("Поиск по категории", [&]() {ProductController::searchProducts<std::string>(&Product::getCategory); }),
-					MenuView("Поиск по марке", [&]() {ProductController::searchProducts<std::string>(&Product::getMark); }),
-					MenuView("Поиск по цене", [&]() {ProductController::searchProducts<double>(&Product::getCost); })
+				MenuView("Поиск по ID", [&]() { _productController.searchProducts<int>(&Product::getId); }),
+					MenuView("Поиск по наименованию", [&]() {_productController.searchProducts<std::string>(&Product::getName); }),
+					MenuView("Поиск по категории", [&]() {_productController.searchProducts<std::string>(&Product::getCategory); }),
+					MenuView("Поиск по марке", [&]() {_productController.searchProducts<std::string>(&Product::getMark); }),
+					MenuView("Поиск по цене", [&]() {_productController.searchProducts<double>(&Product::getCost); })
 			}),
 				MenuView("Сортировка", std::vector<MenuView>{
-				MenuView("Сортировать по ID", [&]() { ProductController::sort<int>(&Product::getId); }),
-					MenuView("Сортировать по наименованию", [&]() {ProductController::sort<std::string>(&Product::getName); }),
-					MenuView("Сортировать по категории", [&]() {ProductController::sort<std::string>(&Product::getCategory); }),
-					MenuView("Сортировать по марке", [&]() {ProductController::sort<std::string>(&Product::getMark); }),
-					MenuView("Сортировать по цене", [&]() {ProductController::sort<double>(&Product::getCost); })
+				MenuView("Сортировать по ID", [&]() { _productController.sort<int>(&Product::getId); }),
+					MenuView("Сортировать по наименованию", [&]() {_productController.sort<std::string>(&Product::getName); }),
+					MenuView("Сортировать по категории", [&]() {_productController.sort<std::string>(&Product::getCategory); }),
+					MenuView("Сортировать по марке", [&]() {_productController.sort<std::string>(&Product::getMark); }),
+					MenuView("Сортировать по цене", [&]() {_productController.sort<double>(&Product::getCost); })
 			})
 		})}) {
 		system("cls");
